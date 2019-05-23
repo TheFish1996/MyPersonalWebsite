4ecaec39b4096a752008c05b65daf33c80f56fa3
Thefish1996 <rfishkin1313@gmail.com>
2019-05-22 23:47:07 -0400

    updated styling for homepage

--- FrontEnd/src/components/Home.js
+++ FrontEnd/src/components/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import NavBar from './Navbar'
+import {Fade} from 'reactstrap'
+
+const video = require("../img/mainTheme.mp4")
 
 function Home(props){
   return (
     <div>
-      <h1>Testing</h1>
+      <video className="video-container" src={video} autoPlay muted loop></video>
+      <NavBar />
     </div>
   )
 }
---
+++ FrontEnd/src/components/Navbar.js
@@ -0,0 +1,51 @@
+import React, {useState} from "react";
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    Fade
+  } from "reactstrap";
+
+  function NavBar(props){
+
+    const [isOpen, setNavbarCollapse] = useState(false)
+
+    function toggle() {
+      setNavbarCollapse(!isOpen)
+    }
+
+      return (
+        <div className="fadeIn">
+          <Navbar dark expand="md">
+            <NavbarBrand href="/" className="navbarTitle mr-auto"><span className="navbarTitle">Jonathan Fishkin</span></NavbarBrand>
+            <NavbarToggler href="/" onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem className="ml-auto">
+                  <NavLink>Home</NavLink>
+                </NavItem>
+                <NavItem className="ml-auto">
+                  <NavLink>Projects</NavLink>
+                </NavItem>
+                <NavItem className="ml-auto">
+                  <NavLink>Technical Skills</NavLink>
+                </NavItem>
+                <NavItem className="ml-auto">
+                  <NavLink>Job Experience</NavLink>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      )
+  }
+
+  export default NavBar;
