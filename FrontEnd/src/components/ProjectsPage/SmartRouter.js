4354b1ba8a26fc56d52d5e1715ba1a8ca2941524
Thefish1996 <rfishkin1313@gmail.com>
2019-06-24 23:39:37 -0400

    updated projects page with first project

--- FrontEnd/src/components/ProjectsPage/SmartRouter.js
+++ FrontEnd/src/components/ProjectsPage/SmartRouter.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { 
     Row, 
     Col,
+    Button
   } from 'reactstrap'
 
   const video = require("../../img/SmartRouterVideo.mp4")
@@ -54,21 +55,77 @@ import {
                 </Row>
             </Col>
             <Col xl="6" className="projectsPageSmartRouter">
-                <Row style={{color: "white"}}>
+                <Row style={{color: "#471218"}}>
                     <h1>Smart Router</h1>
                 </Row>
-                <Row style={{color: "white"}}>
-                    <h3>Description</h3>
+                <Row style={{borderLeft: "3px solid #471218", color: "white"}}>
+                    <h3 style={{paddingLeft: "10px"}}>Description</h3>
                 </Row>
-                <Row style={{backgroundColor: "#471218", color: "white"}}>
+                <Row style={{color: "white"}}>
                     <p>There is a growing demand for router control and network diagnostics among the expanding devices connected to home networks, ranging from smartphones to gaming consoles, tablets and many more. 
                     With all these devices connected, it can be difficult to get the most out of the device, especially when faced with situations like buffering, no internet access, or even loss of connection. 
                     The typical user who has this problem would be anyone with a home router/network that they can’t monitor effectively. 
                     My team of fellow classmates and I helped create the Smart Router app, which takes care of these issues by allowing users to see which device is utilizing the most bandwidth on the network and potentially choosing which device or application takes priority above the rest. 
                     For this project I was responsible for building the actual app interface and working on backend golang scripting.</p>
                 </Row>
+                <Row style={{borderLeft: "3px solid #471218", color: "white"}}>
+                    <h3 style={{paddingLeft: "10px"}}>Technologies Used</h3>
+                </Row>
                 <Row style={{color: "white"}}>
-                    <h3>Technologies Used</h3>
+                    <p>The mobile application was built using React-Native, a framework to build android and iOS apps simulatenously, 
+                    and our backend signaling server was built using Golang with MongoDB as our storage database. We were looking to deploy this database via AWS storage buckets. The router itself
+                    was running an open source linux kurnel called OpenWrt.
+                  </p>
+                </Row>
+                <Row style={{borderLeft: "3px solid #471218", color: "white"}}>
+                    <h3 style={{paddingLeft: "10px"}}>Current Screen:  
+                    {
+                        viewingElement === 1 && <span className="projectsPageDescription" style={{color: "#471218"}}> Main Page</span>
+                    }
+                    {
+                        viewingElement === 2 && <span className="projectsPageDescription" style={{color: "#471218"}}> Router Settings Page</span>
+                    }
+                    {
+                        viewingElement === 3 && <span className="projectsPageDescription" style={{color: "#471218"}}> Live Update Page</span>
+                    }
+                    {
+                        viewingElement === 4 && <span className="projectsPageDescription" style={{color: "#471218"}}> Device Settings Page</span>
+                    }
+                    {
+                        viewingElement === 5 && <span className="projectsPageDescription" style={{color: "#471218"}}> Live Video Demo</span>
+                    }
+                    </h3>
+                </Row>
+                <Row style={{color: "white"}}>
+                    {
+                        viewingElement === 1 && <p className="projectsPageDescription">The current screen is Main Page Screen on the app. This screen shows all the current devices connected to the router. Upon expansion of the module we have data 
+                        provided to us from the server showcasing each specific devices Mac Adress, IP Adress, Average Upload Rate, and Average Download Rate. If you click on the 'Edit Device Settings'
+                        button it will bring you to a more advanced customization for the specific device. </p>
+                    }
+                    {
+                        viewingElement === 2 && <p className="projectsPageDescription">The current screen is the Router Settings Screen on the app. This screen shows the router algorithm mode you can select. Depending on the algorithm selected
+                        you can then chose a Rate Cap for the router or the Rate Cap will be disabled because of the specific algorithm chosen.
+                        </p>
+                    }
+                    {
+                        viewingElement === 3 && <p className="projectsPageDescription">The current screen is the Live Update Screen on the app. This page lists all the current devices on the network and their respected Download Rate and
+                        Upload Rate. Since this is a live page, every 3 seconds the counter will refresh with new data to get a real time effect for the devices on the network. The reason it's 3 seconds is because
+                        on the backend it takes 2 seconds for our server to pull in new data from the router with a 1 second buffer for displaying
+                        </p>
+                    }
+                    {
+                        viewingElement === 4 && <p className="projectsPageDescription">The current screen is the Device Settings Screen on the app. This screen is accessed from the main screen, where you click on a specific devices 'Edit Device Settings'
+                        button. On this screen you can edit the device name and then chose the device class settings. Depending on the algorithm and rate cap chosen for the router settings, you can then modify the 
+                        rate, ceiling, and priority per device.
+                        </p>
+                    }
+                    {
+                        viewingElement === 5 && <p className="projectsPageDescription">This is a live demo of the app being used.
+                        </p>
+                    }
+                </Row>
+                <Row style={{color: "white"}}>
+                    <Button size="md" href="https://github.com/TheFish1996/SmartRouter" style={{backgroundColor: "#471218", marginLeft: "10px"}}>Github Link</Button>
                 </Row>
             </Col>
         </Row>
