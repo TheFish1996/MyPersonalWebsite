18d30d4b486b6703336bfdec663ab4b1e9873b5c
Thefish1996 <rfishkin1313@gmail.com>
2019-07-05 01:09:19 -0400

    updated navigation page

--- FrontEnd/src/components/NavigationPage.js
+++ FrontEnd/src/components/NavigationPage.js
@@ -7,20 +7,45 @@ import {
   } from "reactstrap";
 import {Link} from 'react-router-dom'
 
+const wavesFront = require("../img/waves-front.svg")
+const wavesRear = require("../img/waves-rear.svg")
+const paperBoat = require("../img/paperboat.svg")
+
 function NavigationPage(props){
   return (
-      <Row>
-          <h1>TESTING ITS ME</h1>
-          <h1>TESTING ITS ME</h1>
-          <h1>TESTING ITS ME</h1>
-          <h1>TESTING ITS ME</h1>
-          <h1>TESTING ITS ME</h1>
-          <h1>TESTING ITS ME</h1>
-          <h1>TESTING ITS ME</h1>
-          <h1>TESTING ITS ME</h1>
-          <h1>TESTING ITS ME</h1>
-          <h1>TESTING ITS ME</h1>
-      </Row>
+    <div>
+    <Row>
+      <p>Testing</p>
+    </Row>
+    <Row>
+      <p>Testing</p>
+    </Row>
+    <Row>
+      <p>Testing</p>
+    </Row>
+    <Row>
+      <p>Testing</p>
+    </Row>
+    <Row>
+      <p>Testing</p>
+    </Row>
+    <Row>
+      <p>Testing</p>
+    </Row>
+    <Row>
+      <div class="ocean">
+        <div class="boat-animation">
+          <img alt="Wellen" src={wavesRear} class="waves waves--rear"></img>
+          <div class="paperboat">
+            <div class="paperboat__sink">
+              <img alt="PaperBoat" width="110" height="71" src={paperBoat} class="paperboat__boat" id="paperboat" ></img>
+            </div>
+          </div>
+          <img alt="Wellen" src={wavesFront} class="waves waves--front"></img>
+        </div>
+      </div>
+    </Row>
+    </div>
   )
 }
 
