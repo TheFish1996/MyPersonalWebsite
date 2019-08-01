4eaf7ea4b13b59f8a7c7556d86473ea2e0746117
Thefish1996 <rfishkin1313@gmail.com>
2019-08-01 13:42:56 -0400

    updated home page styling

--- FrontEnd/src/components/HomePage/Animal.js
+++ FrontEnd/src/components/HomePage/Animal.js
@@ -93,21 +93,34 @@ function Animal(props){
                 }} />
             </Col>
             <Col style={{paddingRight: "200px", textAlign: "left", marginTop: "450px"}} xl="4">
-                <p style={{lineHeight: "30px"}}><span style={{fontSize: "60px"}}>A</span>t Stevens, my main projects were either web development or mobile app development. 
-                The projects ranged from accessing spotify's API to build a web app to a full react native mobile application that interfaced with a router
-                I love both web development and mobile development and would enjoy working on either front end or back end
+                <p style={{lineHeight: "30px"}}>
+                    <span style={{fontSize: "60px"}}>A</span>
+                    <span className="aboutMeProjectsText">
+                        t Stevens, my main projects were either web development or mobile app development. 
+                        The projects ranged from accessing spotify's API to build a web app to a full react native mobile application that interfaced with a router
+                        I love both web development and mobile development and would enjoy working on either front end or back end
+                    </span>
                 </p>
             </Col>
             <Col style={{paddingRight: "200px", textAlign: "left", marginBottom: "350px"}} xl="4">
-                <p style={{lineHeight: "30px"}}><span style={{fontSize: "60px"}}>H</span>ello, my name is Jonathan Fishkin and I was born and raised in Staten Island New York. My parents originally come from Israel and Russia.
-                I have always been in love with software engineering / development since highschool and have developed some great projects along the way.
-                The projects I have worked on can be found under the 'Projects' tab
+                <p style={{lineHeight: "30px"}}>
+                    <span style={{fontSize: "60px"}}>H</span>
+                    <span className="aboutMeProjectsText">
+                        ello, my name is Jonathan Fishkin and I was born and raised in Staten Island New York. My parents originally come from Israel and Russia.
+                        I have always been in love with software engineering / development since highschool and have developed some great projects along the way.
+                        The projects I have worked on can be found under the 'Projects' tab
+                    </span>
                 </p>
             </Col>
             <Col style={{paddingRight: "70px", textAlign: "left", marginTop: "450px", paddingLeft: "150px"}} xl="4">
-                <p style={{lineHeight: "30px"}}><span style={{fontSize: "60px"}}>I</span>code in JavaScript, Node.js, C#, Java, HTML/CSS, React.js, .NET, and SQL
-                I am familiar with cloud services pertaining to Amazon Web Services and Google Cloud Platform.
-                A full list of my technical skills is available on my LinkedIN as well as my resume. Which can be found on either the contact page or the navigation page  
+                <p style={{lineHeight: "30px"}}>
+                    <span style={{fontSize: "60px"}}>I</span>
+                    <span className="aboutMeProjectsText">
+                        code in JavaScript, Node.js, C#, Java, HTML/CSS, React.js, .NET, and SQL
+                        I am familiar with cloud services pertaining to Amazon Web Services and Google Cloud Platform.
+                        A full list of my technical skills is available on my LinkedIN as well as my resume. 
+                        hich can be found on either the contact page or the navigation page
+                    </span> 
                 </p>
             </Col>
         </Row>
